35becd9aebde46fdbad856b5c3a493895b2f474d
martamgesha <161434671+martamgesha@users.noreply.github.com>
2025-05-16 00:52:13 +0700

    Create mongo-init.js

---
+++ mongo/mongo-init.js
@@ -0,0 +1,21 @@
+// mongo-init.js
+
+db = db.getSiblingDB('SkinSight');
+
+db.createCollection('users');
+
+db.users.createIndex({ "username": 1 }, { unique: true });
+db.users.createIndex({ "email": 1 }, { unique: true });
+db.users.createIndex({ "user_id": 1 }, { unique: true });
+
+db.users.insertOne({
+  user_id: "10000001",
+  username: "admin",
+  password: "admin123",
+  email: "admin@example.com",
+  role: "admin"
+});
+
+db.createCollection('processes');
+
+db.processes.createIndex({ "user_id": 1 });
