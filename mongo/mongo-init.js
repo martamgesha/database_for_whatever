ee47e01bc3a9071acd519c6e53ce945bb8f92606
martamgesha <161434671+martamgesha@users.noreply.github.com>
2025-05-16 13:22:29 +0700

    Update mongo-init.js

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -6,7 +6,7 @@ db.createCollection('users');
 
 db.users.createIndex({ "username": 1 }, { unique: true });
 db.users.createIndex({ "email": 1 }, { unique: true });
-db.users.createIndex({ "user_id": 10000001 }, { unique: true });
+db.users.createIndex({ "user_id": 1 }, { unique: true });
 
 db.users.insertOne({
   user_id: "10000001",
